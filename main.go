fee98e984772b37a57e2d61e45cf76ae6d7dd5a2
MisoHaUmi <m19151@g.metro-cit.ac.jp>
2023-01-10 13:56:25 +0900

    add workflow

--- main.go
+++ main.go
@@ -16,7 +16,7 @@ func main() {
 
 func Hello() echo.HandlerFunc {
     return func(c echo.Context) error {     
-        return c.String(http.StatusOK, "4441: おでんは大根やん")
+        return c.String(http.StatusOK, "4441: おでんは大根がいい")
     }
 }
 
